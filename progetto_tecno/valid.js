6a159297f55afe6f8ee25da6dc55377c58bacef3
Matteo Porta <matteoimario@gmail.com>
2021-05-18 16:43:10 +0200

    continuazione codice js per controllo credenziali

--- progetto_tecno/valid.js
+++ progetto_tecno/valid.js
@@ -1,81 +1,33 @@
-	//Validtion Code For Inputs
+/*function vai(){
+	var mail=document.getElementById("email").value;
+	alert(mail);
+	$.ajax({
+		type: "GET",
+		url: "data.csv",
+		dataType: "text",
+		cache: false,
+		success: function(data) {
+			alert("ok");
+		}
+		
+	});
+}*/
 
-var email = document.forms['form']['email'];
-var password = document.forms['form']['password'];
 
-var email_error = document.getElementById('email_error');
-var pass_error = document.getElementById('pass_error');
 
-email.addEventListener('textInput', email_Verify);
-password.addEventListener('textInput', pass_Verify);
+$(document).ready(function () {
+	$('btnAccedi').click(function(){
+		$.ajax({
+			url: "data.csv",
+			dataType: "text",
+			success: function (data) {				
+				var lines = data.split(/\r?\n|\r/);
 
-function validated(){
-	if (email.value.length < 9) {
-		email.style.border = "1px solid red";
-		email_error.style.display = "block";
-		email.focus();
-		return false;
-	}
-	if (password.value.length < 6) {
-		password.style.border = "1px solid red";
-		pass_error.style.display = "block";
-		password.focus();
-		return false;
-	}
+				var entries = lines[0].split(',');
+				
+				alert(entries);
+			}
+		});
+	});
 
-}
-function email_Verify(){
-	if (email.value.length >= 8) {
-		email.style.border = "1px solid silver";
-		email_error.style.display = "none";
-		return true;
-	}
-}
-function pass_Verify(){
-	if (password.value.length >= 5) {
-		password.style.border = "1px solid silver";
-		pass_error.style.display = "none";
-		return true;
-	}
-}
-
-/*$(document).ready(function controllo() {
-    $.ajax({
-        type: "GET",
-        url: "data.txt",
-        dataType: "text",
-        success: function(data) {processData(data);}
-     });
-});*/
-
-function processData(allText) {
-	
-	//1 campo-->email
-	//2 campo-->password
-    //3 campo-->nome
-    //4 campo-->cognome
-    var linee = allText.split(/\r\n|\n/);
-	var controllo =false;
-	var emailinserita=document.getElementById("email").value;
-	var passwordinserita=document.getElementById("password").value;
-	for(var i=0; i<linee.length&&!controllo;i++)
-	{
-		var campo = linee[i].split(';');
-		var email=campo[0];
-		var password=campo[1];
-		if(emailinserita==email&&passwordinserita==password)
-		{
-			controllo=true;
-		}		
-	}
-	
-	if(controllo)
-	{
-		alert("valido");
-	}
-	else{
-		alert("non valido");
-	}
-	
-    
-}
+});
