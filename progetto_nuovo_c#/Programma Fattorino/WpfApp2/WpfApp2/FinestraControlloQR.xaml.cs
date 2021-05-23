19526b4351f6953528fc0cf8f5e5a6ddc7c003f5
Luca Lauria <lucalau0405@gmail.com>
2021-05-23 17:38:51 +0200

    INIZIATO PROGRAMMA FATTORINO Inserimento password per accedere alle varie funzionalità Lettura codice qr dalla webcam grazie al pacchetto installato (QRwebcam)

---
+++ progetto_nuovo_c#/Programma Fattorino/WpfApp2/WpfApp2/FinestraControlloQR.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Logica di interazione per FinestraControlloQR.xaml
+    /// </summary>
+    public partial class FinestraControlloQR : Window
+    {
+        public FinestraControlloQR()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //mette nella combobox i nomi della camera attive sul pc quando avvio la finestra
+            camSelect.ItemsSource = webCam.CameraNames;
+        }
+
+        private void QrWebCamControl_QrDecoded(object sender, string e)
+        {
+            //mette la scritta presente nel qr nella textbox  sottostante
+            dtext.Text = e;
+        }
+
+        private void camSelect_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //quando seleziono una camera dalla combo prende la posizione della camera che scelgo (es ho una camera prende l'indice 0)
+            webCam.CameraIndex = camSelect.SelectedIndex;
+        }
+    }   
+}
---
+++ progetto_nuovo_c#/Programma Fattorino/WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string password = "123";
+        public MainWindow()
+        {
+            InitializeComponent();
+            /*OpenFileDialog open = new OpenFileDialog();
+             if (open.ShowDialog() == true)
+             {
+                 g.setnomefile(open.FileName);
+                 g.CaricaFile();
+             }*/
+        }
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (Password_box.Password == password)
+            {
+                MessageBox.Show("PASSWORD CORRETTA");
+
+                FinestraControlloQR tmp = new FinestraControlloQR();
+                this.Hide();
+                tmp.Show();
+
+            }
+            else
+            {
+                MessageBox.Show("PASSWORD ERRATA");
+            }
+        }
+    }
+}
